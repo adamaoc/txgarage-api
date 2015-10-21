637d206df82f92f370a35a2da2bcd1472eb1f920
adam moore <adamaoc@gmail.com>
2015-10-20 19:47:55 -0500

    adding instagram and facebook to stats model

--- app/models/StatsModel.php
+++ app/models/StatsModel.php
@@ -3,18 +3,22 @@
 class StatsModel
 {
   private $_twitterFollowers = null;
+  private $_facebookLikes = null;
+  private $_instgramFollows = null;
 
   public function __construct()
   {
       $this->_getTwitter();
+      $this->_getFacebook();
+      $this->_getInstagram();
   }
 
   public function getAll()
 	{
     $stats = array(
-      array("id"=>1004, "title"=>"Facebook", "slug"=>"facebook", "stat"=>866, "color"=>"blue", "link"=>"http://facebook.com/txgarage"),
+      array("id"=>1004, "title"=>"Facebook", "slug"=>"facebook", "stat"=>$this->_facebookLikes, "color"=>"blue", "link"=>"http://facebook.com/txgarage"),
       array("id"=>1005, "title"=>"Twitter", "slug"=>"twitter", "stat"=>$this->_twitterFollowers, "color"=>"light-blue","link"=>"http://twitter.com/txgarage"),
-      array("id"=>1006, "title"=>"Instagram", "slug"=>"instagram", "stat"=>483, "color"=>"green", "link"=>"http://instagram.com/txgarage")
+      array("id"=>1006, "title"=>"Instagram", "slug"=>"instagram", "stat"=>$this->_instgramFollows, "color"=>"green", "link"=>"http://instagram.com/txgarage")
     );
 		return $stats;
 	}
@@ -27,11 +31,16 @@ class StatsModel
 
   private function _getFacebook()
   {
-
+    $data = file_get_contents('https://graph.facebook.com/txgarage/?fields=likes&access_token='.Config::get('facebook/access_token'));
+    $data = json_decode($data);
+    $this->_facebookLikes = $data->likes;
   }
 
   private function _getInstagram()
   {
+    $data = file_get_contents('https://api.instagram.com/v1/users/1648915262/?client_id='.Config::get('instagram/client_id'));
+    $data = json_decode($data);
+    $this->_instgramFollows = $data->data->counts->followed_by;
 
   }
 
