2c1dd1108a4f399c2ce8a020a9f3481bd31fba91
adam moore <adamaoc@gmail.com>
2015-11-24 06:38:45 -0600

    added views endpoint

---
+++ app/controllers/views.php
@@ -0,0 +1,37 @@
+<?php
+
+class Views extends Controller
+{
+
+  public function index()
+  {
+    $monthArr = array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
+    $statsArr2016 = array('jan' => 11652,
+                          'feb' => 10524,
+                          'mar' => 9862,
+                          'apr' => 10010,
+                          'may' => 9952,
+                          'jun' => 9956,
+                          'jul' => 9614,
+                          'aug' => 10340,
+                          'sep' => 10928,
+                          'oct' => 11045,
+                          'nov' => 7012,
+                          'dec' => 0);
+    $stats = array(
+      'labels'  => $monthArr,
+      'datasets' => array(
+        array(
+          'label' => 'Views Per Month',
+          'fillColor' => "rgba(220,220,220,0.8)",
+          'strokeColor' => "rgba(220,220,220,0.8)",
+          'highlightFill' => "rgba(45,124,185,0.75)",
+          'highlightStroke' => "rgba(220,220,220,1)",
+          'data' => $statsArr2016
+        )
+      )
+    );
+		$this->api($stats, 'views');
+  }
+
+}
