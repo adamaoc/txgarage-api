eafff1ac66c3c4b11529092ff068d8a5c7525e05
Adam Moore <adamaoc@gmail.com>
2015-12-07 22:49:13 -0600

    Updating Dec. stats

--- app/controllers/views.php
+++ app/controllers/views.php
@@ -17,7 +17,7 @@ class Views extends Controller
                           'sep' => 10928,
                           'oct' => 11045,
                           'nov' => 8546,
-                          'dec' => 1130);
+                          'dec' => 2334);
     $stats = array(
       'id' => 1001,
       'labels'  => $monthArr,
