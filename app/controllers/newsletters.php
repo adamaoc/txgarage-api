33cd937e0d288c898577bd4e3c9453eb0909b620
adam moore <adamaoc@gmail.com>
2016-07-26 18:32:24 -0500

    updated api for newsletters

--- app/controllers/newsletters.php
+++ app/controllers/newsletters.php
@@ -6,9 +6,31 @@ class Newsletters extends Controller
   public function index()
   {
     $newsletterModel = $this->model('NewsletterModel');
-    $stats = $newsletterModel->getAll();
+    $stats = $newsletterModel->getRecent();
 
 		$this->api($stats, 'newsletter');
+  }
+
+  public function all()
+  {
+    $newsletterModel = $this->model('NewsletterModel');
+    $stats = $newsletterModel->getAll();
+
+		$this->api($stats, 'newsletters');
+  }
+
+  public function post()
+  {
+    header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key");
+
+    $json = @file_get_contents('php://input');
+    $array = json_decode($json, true);
+
+    $newsletterModel = $this->model('NewsletterModel');
+    $newsletter = $newsletterModel->postNew($array);
+
+    $this->api($newsletter, 'newsletter', 201);
   }
 
 }
--- app/models/NewsletterModel.php
+++ app/models/NewsletterModel.php
@@ -8,8 +8,34 @@ class NewsletterModel
   public function getAll()
 	{
     $this->_db = DB::getInstance();
-		$data = $this->_db->query('SELECT * FROM newsletter');
+		$data = $this->_db->query('SELECT * FROM newsletters');
 		$data = $data->results();
 		return $data;
 	}
+  public function getRecent()
+  {
+    $this->_db = DB::getInstance();
+		$data = $this->_db->query('SELECT * FROM newsletters ORDER BY ID DESC LIMIT 1');
+		$data = $data->results();
+		return $data[0];
+  }
+
+  public function postNew($payload)
+	{
+    $newsletter = $payload['data'];
+    $this->_db = DB::getInstance();
+    $newsletter['updated'] = date('Y-m-d');
+    $this->_db->insert('newsletters', $newsletter);
+
+    return $newsletter;
+  }
 }
+
+// JSON example // 
+// {
+//     "data": {
+//         "subscribers": 85,
+//         "campaigns": 102,
+//         "opens": 44.3
+//     }
+// }
