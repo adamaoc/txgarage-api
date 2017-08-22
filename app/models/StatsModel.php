c2df9f23dd73ed92e3a687d0048b39aafc31794d
Adam Moore <adamaoc@gmail.com>
2017-08-22 11:54:31 -0500

    updated stats with youtube

--- app/models/StatsModel.php
+++ app/models/StatsModel.php
@@ -4,12 +4,14 @@ class StatsModel
 {
   private $_twitterFollowers = null;
   private $_facebookLikes = null;
-  private $_instgramFollows = 784;
+  private $_instgramFollows = 755;
+  private $_youtubeNumbers = null;
 
   public function __construct()
   {
       $this->_getTwitter();
       $this->_getFacebook();
+      $this->_getYouTubeSubscribers();
       // $this->_getInstagram(); need to fix api...
   }
 
@@ -18,7 +20,8 @@ class StatsModel
     $stats = array(
       array("id"=>1004, "title"=>"Facebook", "slug"=>"facebook", "stat"=>$this->_facebookLikes, "color"=>"blue", "link"=>"http://facebook.com/txgarage"),
       array("id"=>1005, "title"=>"Twitter", "slug"=>"twitter", "stat"=>$this->_twitterFollowers, "color"=>"light-blue","link"=>"http://twitter.com/txgarage"),
-      array("id"=>1006, "title"=>"Instagram", "slug"=>"instagram", "stat"=>$this->_instgramFollows, "color"=>"green", "link"=>"http://instagram.com/txgarage")
+      array("id"=>1006, "title"=>"Instagram", "slug"=>"instagram", "stat"=>$this->_instgramFollows, "color"=>"green", "link"=>"http://instagram.com/txgarage"),
+      array("id"=>1006, "title"=>"YouTube", "slug"=>"youtube", "stat"=>$this->_youtubeNumbers, "color"=>"red", "link"=>"http://youtube.com/user/texasgarage")
     );
 		return $stats;
 	}
@@ -41,7 +44,23 @@ class StatsModel
     $data = file_get_contents('https://api.instagram.com/v1/users/1648915262/?client_id='.Config::get('instagram/client_id'));
     $data = json_decode($data);
     $this->_instgramFollows = $data->data->counts->followed_by;
+  }
+  private function _getYouTubeSubscribers()
+  {
+    // Change channelid value to match your YouTube channel ID
+    $url = 'https://www.youtube.com/subscribe_embed?channelid=UCckngdLzBfaBKqcjj3VkxcA';
 
+    // Fetch the Subscribe button HTML
+    $button_html = file_get_contents( $url );
+
+    // Extract the subscriber count
+    $found_subscribers = preg_match( '/="0">(\d+)</i', $button_html, $matches );
+
+    if ( $found_subscribers && isset( $matches[1] ) ) {
+        $this->_youtubeNumbers = intval( $matches[1] );
+    } else {
+      $this->_youtubeNumbers = 'error';
+    }
   }
 
 }
