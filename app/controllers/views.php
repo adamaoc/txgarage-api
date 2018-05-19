ae979a7093b9e9437d32b95d52a8ec97a11e5cbe
Adam Moore <adamaoc@gmail.com>
2018-05-18 22:05:24 -0500

    pulling views from asset file

--- app/controllers/views.php
+++ app/controllers/views.php
@@ -7,93 +7,9 @@ class Views extends Controller
   {
     // $viewsModel = $this->model('ViewsModel');
     // $stats = $viewsModel->getAll();
-
-    $stats = array(
-      array(
-        "id" => "1",
-        "year" => "2018",
-        "month" => "Jan",
-        "views" => '9847',
-        "slugID" => "2018_Jan"
-      ),
-      array(
-        "id" => "2",
-        "year" => "2018",
-        "month" => "Feb",
-        "views" => '13080',
-        "slugID" => "2018_Feb"
-      ),
-      array(
-        "id" => "3",
-        "year" => "2018",
-        "month" => "Mar",
-        "views" => '15208',
-        "slugID" => "2018_Mar"
-      ),
-      array(
-        "id" => "4",
-        "year" => "2018",
-        "month" => "Apr",
-        "views" => '13950',
-        "slugID" => "2018_Apr"
-      ),
-      array(
-        "id" => "5",
-        "year" => "2018",
-        "month" => "May",
-        "views" => '7279',
-        "slugID" => "2018_May"
-      ),
-      array(
-        "id" => "6",
-        "year" => "2018",
-        "month" => "June",
-        "views" => '0',
-        "slugID" => "2018_June"
-      ),
-      array(
-        "id" => "7",
-        "year" => "2018",
-        "month" => "July",
-        "views" => '0',
-        "slugID" => "2018_July"
-      ),
-      array(
-        "id" => "8",
-        "year" => "2018",
-        "month" => "Aug",
-        "views" => '0',
-        "slugID" => "2018_Aug"
-      ),
-      array(
-        "id" => "9",
-        "year" => "2018",
-        "month" => "Sept",
-        "views" => '0',
-        "slugID" => "2018_Sept"
-      ),
-      array(
-        "id" => "10",
-        "year" => "2018",
-        "month" => "Oct",
-        "views" => '0',
-        "slugID" => "2018_Oct"
-      ),
-      array(
-        "id" => "11",
-        "year" => "2018",
-        "month" => "Nov",
-        "views" => '0',
-        "slugID" => "2018_Nov"
-      ),
-      array(
-        "id" => "12",
-        "year" => "2018",
-        "month" => "Dec",
-        "views" => '0',
-        "slugID" => "2018_Dec"
-      )
-    );
+    $stats = file_get_contents("http://assets.txgarage.com/webviews.json");
+    $stats = json_decode($stats);
+    
 		$this->api($stats, 'views');
   }
 
