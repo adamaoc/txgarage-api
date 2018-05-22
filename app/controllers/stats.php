db4920d6da15e746f165e423bffd6f158d43a8ce
Adam Moore <adamaoc@gmail.com>
2018-05-22 13:38:34 -0500

    Update stats.php
    updating to point to new assets json

--- app/controllers/stats.php
+++ app/controllers/stats.php
@@ -5,9 +5,11 @@ class Stats extends Controller
 
   public function index()
   {
-    $statsModel = $this->model('StatsModel');
-    $stats = $statsModel->getAll();
-		$this->api($stats, 'stats');
+//     $statsModel = $this->model('StatsModel');
+//     $stats = $statsModel->getAll();
+    $stats = file_get_contents("http://assets.txgarage.com/webstats.json");
+    $stats = json_decode($stats);
+    $this->api($stats, 'stats');
   }
 
 }
